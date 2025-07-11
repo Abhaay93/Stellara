d8402015eab2770916d275fc06251451e70e9236
Abhaay93 <abhayhipparkar97@gmail.com>
2025-07-11 22:40:01 +0530

    Updated sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,32 @@
+let symmetry = 6;       // Number of symmetrical slices
+let angleStep;          // Angle between each slice
+
 function setup() {
-    createCanvas(windowWidth, windowHeight);  // full screen
-    background(0);                            // black background
-    stroke(255);                              // white stroke
-    strokeWeight(4);                          // line thickness
+    createCanvas(windowWidth, windowHeight);
+    angleMode(DEGREES);  // Angle unit = degrees
+    background(0);
+    stroke(255);
+    strokeWeight(2);
+    angleStep = 360 / symmetry;
 }
 
 function draw() {
+    translate(width / 2, height / 2);  // Center of canvas
+
     if (mouseIsPressed) {
-        line(pmouseX, pmouseY, mouseX, mouseY);  // draw line from previous to current mouse
+        let mx = mouseX - width / 2;
+        let my = mouseY - height / 2;
+        let pmx = pmouseX - width / 2;
+        let pmy = pmouseY - height / 2;
+
+        for (let i = 0; i < symmetry; i++) {
+            rotate(angleStep);
+            line(mx, my, pmx, pmy);
+
+            push();               // Mirror drawing
+            scale(1, -1);
+            line(mx, my, pmx, pmy);
+            pop();
+        }
     }
 }
